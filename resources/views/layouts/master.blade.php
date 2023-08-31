050dbdf427bcf2820c3eec19282b448baad3a6be
Aldy Aditia Hidayat <aschentegeon@gmail.com>
2023-08-31 17:03:48 +0700

    fix: Login

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -65,7 +65,7 @@
           <img src="{{asset('assets/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Username</a>
+          <a href="#" class="d-block">{{ Auth()->User()->username }}</a>
         </div>
       </div>
 
